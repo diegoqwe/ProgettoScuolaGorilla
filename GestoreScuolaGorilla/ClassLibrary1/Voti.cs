156e0d92a7148c1bcb411db5d5191a0aadd90162
user4_12_1 <user4_12_1@LENOVO56>
2023-06-07 18:00:38 +0200

    Creazione classe voti

---
+++ GestoreScuolaGorilla/ClassLibrary1/Voti.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary1
+{
+    public class Voti
+    {
+        protected List<double> voto;
+
+        public Voti()
+        {
+            voto = new List<double>();
+        }
+
+        public void Aggiungi(double v)
+        {
+            voto.Add(v);
+        }
+
+
+    }
+}
