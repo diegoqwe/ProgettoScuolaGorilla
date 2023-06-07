8b6635faafe73779eb55e36436bc9c58a461c8eb
binisammy <samubini01@gmail.com>
2023-06-07 17:11:34 +0200

    server updates

---
+++ GestoreScuolaGorilla/ModuloServer/Controllers/ControllerAPI.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello World");
+        }
+
+        //http get se viene richiesto di restituire il numero 50
+        [HttpGet("{id}")]
+        public ActionResult<string> GetInt(int id)
+        {
+            return Ok($"You requested with this id: {id}");
+        }
+    }
+}
